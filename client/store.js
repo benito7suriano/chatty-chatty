dbfc119f6173c206fcce21896c5da6ce71e53c62
Beno Suriano <bjsuriano@gmail.com>
2019-08-24 10:20:33 -0600

    Badge is connected and shows actual num of messages in channel.

--- client/store.js
+++ client/store.js
@@ -1,35 +1,52 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import axios from 'axios'
 
 // initial state
-const initialState = { messages: [] }
+const initialState = {
+  messages: [],
+  userInput: ''
+}
 
 // action types
 const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER'
+const WRITING_MESSAGE = 'WRITING_MESSAGE'
+const RECEIVED_NEW_MESSAGE = 'RECEIVED_NEW_MESSAGE'
+// const GOT_CHANNELS_FROM_SERVER = 'GOT_CHANNELS_FROM_SERVER'
 
 // action creators
 export const gotMessagesFromServer = (messages) => ({type: GOT_MESSAGES_FROM_SERVER, messages})
 
+// export const gotChannelsFromServer = channels => ({type: GOT_CHANNELS_FROM_SERVER, channels})
+
 // thunk creators
 export const fetchMessages = () => async (dispatch) => {
-  console.log('Dispatched thunk')
   const response = await axios.get('/api/messages')
   const messages = response.data
+
   const action = gotMessagesFromServer(messages)
   dispatch(action)
 }
+
+// export const fetchChannels = () => async dispatch => {
+//   const { data } = await axios.get('/api/channels')
+//   const action = gotChannelsFromServer(data)
+//   dispatch(action)
+// }
 
 // reducer
 const reducer = (state=initialState, action) => {
   switch(action.type) {
     case GOT_MESSAGES_FROM_SERVER:
       return {...state, messages: action.messages }
+    case GOT_CHANNELS_FROM_SERVER:
+      return {...state, channels: action.channels }
     default:
       return state
   }
 }
 
 // store
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(logger, thunk))
 export default store
